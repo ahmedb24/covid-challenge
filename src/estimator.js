47ddf532569f7e25655e971f936d4e32ecef7126
mdz24 <kbbello2495@gmail.com>
2020-04-14 00:28:24 +0100

    first-covid-challenge

--- src/estimator.js
+++ src/estimator.js
@@ -4,47 +4,48 @@ const estimateCurrentlyInfected = (data) => {
 
 
 const estimateCurrentlyInfectedImpact = (data) => {
-    if(data.reportedCases==''||data.reportedCases==0){
+  var currentlyInfectedImpact;    
+  if(data.reportedCases === '' || data.reportedCases==0) {
     return 0;
-    }
-    var currentlyInfectedImpact = data.reportedCases*10;
-    return currentlyInfectedImpact;
+  }
+  currentlyInfectedImpact = data.reportedCases*10;
+  return currentlyInfectedImpact;
 };
       
 const estimateCurrentlyInfectedSevereImpact = (data) => {
-    if(data.reportedCases==''||data.reportedCases==0){
+  if(data.reportedCases==''||data.reportedCases==0){
     return 0;
-    }
-    var currentlyInfectedSevereImpact = data.reportedCases*50;
-    return currentlyInfectedSevereImpact;
+  }
+  var currentlyInfectedSevereImpact = data.reportedCases*50;
+  return currentlyInfectedSevereImpact;
 };
         
-      const estimateCurrentlyInfectedByRequestedTimeImpact = (data, days) => {
-          var days = 30;
-          var factor = 30/3;
-          var infectionInThirtyDays = estimateCurrentlyInfectedImpact(data) * Math.pow(2,factor);
-          return infectionInThirtyDays;
-      };
+const estimateCurrentlyInfectedByRequestedTimeImpact = (data, days) => {
+  var days = 30;
+  var factor = 30/3;
+  var infectionInThirtyDays = estimateCurrentlyInfectedImpact(data) * Math.pow(2,factor);
+  return infectionInThirtyDays;
+};
         
-      const estimateCurrentlyInfectedByRequestedTimeSevereImpact = (data, days) => {
-          var days = 30;
-          var factor = 30/3;
-          var infectionInThirtyDays = estimateCurrentlyInfectedSevereImpact(data) * Math.pow(2,factor);
-          return infectionInThirtyDays;
-      };
+const estimateCurrentlyInfectedByRequestedTimeSevereImpact = (data, days) => {
+  var days = 30;
+  var factor = 30/3;
+  var infectionInThirtyDays = estimateCurrentlyInfectedSevereImpact(data) * Math.pow(2,factor);
+  return infectionInThirtyDays;
+};
           
-        const covid19ImpactEstimator=(data)=>{  
-            return {
-                data: data,
-                impact: {
-                    currentlyInfected: estimateCurrentlyInfectedImpact(data),
-                    currentlyInfectedByRequestedTime: estimateCurrentlyInfectedByRequestedTimeImpact(data)
-                },
-                severeImpact: {
-                    currentlyInfected: estimateCurrentlyInfectedSevereImpact(data),
-                    currentlyInfectedByRequestedTime: estimateCurrentlyInfectedByRequestedTimeSevereImpact(data)
-                }
-            }
-        };
+const covid19ImpactEstimator=(data)=>{  
+  return {
+    data: data,
+    impact: {
+      currentlyInfected: estimateCurrentlyInfectedImpact(data),
+      currentlyInfectedByRequestedTime: estimateCurrentlyInfectedByRequestedTimeImpact(data)
+    },
+    severeImpact: {
+      currentlyInfected: estimateCurrentlyInfectedSevereImpact(data),
+      currentlyInfectedByRequestedTime: estimateCurrentlyInfectedByRequestedTimeSevereImpact(data)
+    }
+  }
+};
 
 export default covid19ImpactEstimator;
